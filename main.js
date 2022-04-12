c96846087876d2ebac5126b2a859f2a81878a4c1
grinsekatze <violetta.stix@gmail.com>
2022-04-12 12:30:18 +0200

    Added Tools: Pencil, Eraser & Reset

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const grid = document.querySelector(".grid-container");
+const grid = document.querySelector('.grid-container');
+const resetButton = document.querySelector('.reset')
+const eraserButton = document.querySelector('.eraser');
+const colorButton = document.querySelector('.color');
+
 let click = false;
+let tool = 'pencil';
+let color = 'black';
 
 function setupSketchPad(size) {
 
@@ -8,20 +14,39 @@ function setupSketchPad(size) {
 
     for(let i = 0; i < (size * size); i++) {
         let square = document.createElement('div');
-        square.addEventListener('mouseover', checkMouse);
+
+        square.addEventListener('mouseover', useTool);
         square.addEventListener('mouseup', () => click = false );
         square.addEventListener('mousedown', (e) => {
             click = true;
-            checkMouse(e);
+            useTool(e);
         });
+
         grid.insertAdjacentElement('beforeend', square);
+    }
+
+    colorButton.addEventListener('click', () => tool = 'pencil');
+    eraserButton.addEventListener('click', () => tool = 'eraser');
+}
+
+setupSketchPad(16)
+
+function useTool(e) {
+    if (click === true) {
+        if (tool === 'pencil') {
+            e.target.style.backgroundColor = color;
+        } else if (tool === 'eraser') {
+            e.target.style.backgroundColor = 'white';
+        }
     }
 }
 
-setupSketchPad(16);
 
-function checkMouse(e) {
-    if (click === true) {
-        e.target.style.backgroundColor = 'black';
-    }
+
+resetButton.addEventListener('click', resetBoard)
+
+function resetBoard(){
+    //forEach div in grid, add white to whole grid
+    let allSquares = grid.querySelectorAll("div");
+    allSquares.forEach(eachDiv => eachDiv.style.backgroundColor = 'white');
 }
